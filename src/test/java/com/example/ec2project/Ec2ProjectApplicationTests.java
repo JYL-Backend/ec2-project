abb21f1e29f6fbec929522fa05d592c68d565383
appbutton6 <jylim@bsks.ac.kr>
2021-08-31 08:30:26 +0900

    init

---
+++ src/test/java/com/example/ec2project/Ec2ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ec2project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ec2ProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

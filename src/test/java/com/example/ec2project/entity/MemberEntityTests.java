dc5bb946a80d6fa37b7a93e5a6d1c3b9359ec1a0
appbutton6 <jylim@bsks.ac.kr>
2021-08-31 09:30:36 +0900

    ec2 테스트중..2

--- src/test/java/com/example/ec2project/entity/MemberEntityTests.java
+++ src/test/java/com/example/ec2project/entity/MemberEntityTests.java
@@ -2,8 +2,10 @@ package com.example.ec2project.entity;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
+@Transactional
 public class MemberEntityTests {
     @Test
     public void create(){

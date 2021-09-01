7afea377a0f7f8e87e68c4f4fac347e2ab6242a2
appbutton6 <jylim@bsks.ac.kr>
2021-09-01 10:25:30 +0900

    v1 배포 테스트

---
+++ src/main/java/com/example/ec2project/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.example.ec2project.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+    @GetMapping("/verison")
+    public String version(){
+        return "1.0";
+    }
+}
